f206f9248d84adcf69b89111f764bdbb42937cfa
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-12 11:03:16 -0800

    tested API and used Doc to render info on page

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,14 @@
 $.ajax('http://www.omdbapi.com/?t=Goodfellas&apikey=11bc5e4c')
     // Runs when response from the request is successful
     .then(res => {
-        console.log(res.poster)
-
-        $('#poster').attr('src', res.poster)
+        $('#movie').html(`
+        <img src="${res.Poster}" alt="${res.Title}">
+        <h2>${res.Title}</h2>
+        <h3>Directed by: ${res.Director}</h3>
+        <p>${res.Plot}</p>
+        `)
     })
     // Runs when request has an error
-    .catch(err => {
-        console.log(err)
-    })
+    .catch(err => { console.log(err) })
 
 
